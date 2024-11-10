22d82b8379c38cae8925fd1b1b38511c8d069ae6
Ivan Goncharov <ivan.stereotekk@gmail.com>
2024-11-11 02:58:57 +0300

    reorganised data and posts

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -11,7 +11,22 @@ export default function BlogPosts() {
 
   return (
     <section>
-      <h1 className="mb-8 text-2xl font-medium tracking-tight">My Blog</h1>
+      <h1 className="mb-8 text-2xl font-medium tracking-tight">Мой блог</h1>
+
+    <div className="pb-24 prose prose-neutral dark:prose-invert ">
+<p>
+      Прежде чем что-либо делать, я всегда задаю себе вопрос...<br/> 
+      <b>Зачем ?</b>
+    <p>      
+    Изучая материал всегда важно его запомнить, осознать на уровне моторной памяти.
+    Так чтобы оперировать этими знаниями на бесознательном уровне. <br/>
+    А поэтому, я решил конспектировать все то что считаю для себя важным. <br/>
+    Так что в большей степени я пишу это для себя, а если кто то будет пользоваться 
+    моими шпаргалками, или кому то покажется текст и подача супер понятной. <br/>
+    Или вы случайно нашли это место, то как говориться - welcome!
+    </p>
+</p>
+    </div>
       <div>
         {allBlogs
           .sort((a, b) => {
@@ -30,7 +45,7 @@ export default function BlogPosts() {
               href={`/blog/${post.slug}`}
             >
               <div className="w-full flex flex-col sm:flex-row justify-between items-start sm:items-center space-y-1 sm:space-y-0 sm:space-x-2">
-                <p className="text-black dark:text-white tracking-tight">
+                <p className="text-black text-lg not-italic hover:italic dark:text-white tracking-tight">
                   {post.metadata.title}
                 </p>
                 <p className="text-neutral-600 dark:text-neutral-400 tabular-nums text-sm">
--- app/projects/project-data.tsx
+++ app/projects/project-data.tsx
@@ -1,30 +1,30 @@
 export interface Project {
-  title: string;
-  year: number;
-  description: string;
-  url: string;
-}
-
-export const projects: Project[] = [
-  {
-    title: "Тут скоро будут проекты...",
-    year: 2023,
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam veritatis dolorem rem praesentium dicta labore, at laudantium quisquam.",
-    url: "https://example.com/",
-  },
-  {
-    title: "Следующий проект",
-    year: 2022,
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam veritatis dolorem rem praesentium dicta labore, at laudantium quisquam.",
-    url: "https://example.com/",
-  },
-  {
-    title: "Еще один очень интересный",
-    year: 2021,
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam veritatis dolorem rem praesentium dicta labore, at laudantium quisquam.",
-    url: "https://example.com/",
-  },
-];
+    title: string;
+    year: number;
+    description: string;
+    url: string;
+  }
+  
+  export const projects: Project[] = [
+    {
+      title: "Тут скоро будут проекты...",
+      year: 2023,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam veritatis dolorem rem praesentium dicta labore, at laudantium quisquam.",
+      url: "https://example.com/",
+    },
+    {
+      title: "Следующий проект",
+      year: 2022,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam veritatis dolorem rem praesentium dicta labore, at laudantium quisquam.",
+      url: "https://example.com/",
+    },
+    {
+      title: "Еще один очень интересный",
+      year: 2021,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam veritatis dolorem rem praesentium dicta labore, at laudantium quisquam.",
+      url: "https://example.com/",
+    },
+  ];
