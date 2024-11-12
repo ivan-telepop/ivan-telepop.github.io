bdabedce49f1a1f443e1baf7ab25d6fc6d4bd087
Ivan Goncharov <ivan.stereotekk@gmail.com>
2024-11-13 00:10:32 +0300

    small pieces of text added

--- app/projects/project-data.tsx
+++ app/projects/project-data.tsx
@@ -7,11 +7,11 @@ export interface Project {
   
   export const projects: Project[] = [
     {
-      title: "Тут скоро будут проекты...",
+      title: "Жесты рук, Ableton Live, MIDI, Компьютерное зрение.",
       year: 2023,
       description:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam veritatis dolorem rem praesentium dicta labore, at laudantium quisquam.",
-      url: "https://example.com/",
+        "Время от времени я пробую разные технологии. Тут я переобразовал координаты пальцев в MIDI сигнал. Сигнал к аблетону, и это звучит! Конечно проект требует работы, развития, но все же на мой взгляд очень интересный проект. Ну или прототип проекта. Буду продолжать его по мере возможности!",
+      url: "https://youtu.be/wMKDv2Fauus",
     },
     {
       title: "Следующий проект",
