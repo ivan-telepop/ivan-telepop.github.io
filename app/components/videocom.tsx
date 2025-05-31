850137ca4e2840313db32963c9ed264896921c67
Ivan Goncharov <ivan.stereotekk@gmail.com>
2025-05-31 20:37:40 +0400

    video

--- app/components/videocom.tsx
+++ app/components/videocom.tsx
@@ -2,7 +2,8 @@
 export default function VideoYoutube(){
     return <>
     <div>
-<iframe className="w-[280px] h-[157px] sm:w-[374px] sm:h-[210px] lg:w-[560px] lg:h-[315px]" src="https://www.youtube.com/embed/erfhqGZoAJg?si=YyP0HP4SLWUYqxOF" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+<iframe className="w-[280px] h-[157px] sm:w-[374px] sm:h-[210px] lg:w-[560px] lg:h-[315px]" 
+src="https://www.youtube.com/embed/erfhqGZoAJg?si=YyP0HP4SLWUYqxOF" title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe>
 </div>
     </>
 }
