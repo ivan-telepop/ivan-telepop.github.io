1a6b7eec79142d574eeb9613f84aa8152731159c
Ivan Goncharov <ivan.stereotekk@gmail.com>
2025-06-09 00:43:39 +0400

    restructured

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { socialLinks } from "./config";
 import VideoYoutube from "./components/videocom";
+import Link from "next/link";
 
 export default function Page() {
 
@@ -34,10 +35,18 @@ export default function Page() {
 Я занимаюсь разработкой серверных приложений на Python, специализируюсь на создании REST API для бизнеса. 
 Имею опыт в коммерческой разработке, работал в проектах, решая задачи по построению и оптимизации backend-систем и инфраструктуры.<br/>
 
-
-Мой основной стек: Python, FastAPI, Docker, MQ, Relational Databases, SQLAlchemy. 
-Хорошо понимаю архитектуру веб-приложений, работаю с базами данных, настраиваю окружение и деплой. 
-Есть опыт работы с LLM - Ollama, LangChain, OpenAI - RAG / Function Calling / Completions. 
+<pre className="prose prose-neutral dark:prose-invert ">
+Мой основной стек: <br/>
+Python, FastAPI, Docker, MQ, <br/>
+Relational Databases, SQLAlchemy. <br/>
+Хорошо понимаю архитектуру веб-приложений <br/>
+Работаю с базами данных, настраиваю окружение и деплой. <br/>
+Есть опыт работы с LLM <br/> 
+Ollama, LangChain, OpenAI <br/>
+RAG / Function Calling / Completions. <br/>
+<p>Разрабатываю <Link href="https://t.me/ewanG808" className=" p-1 no-underline bg-emerald-400 dark:bg-blue-500">frontend</Link></p> <br/>
+React / Next.js / Tailwind и др.<br/>
+</pre>
 
 <p>Так же имею опыт работы сетевым инженером, что помогает мне лучше разбираться в инфраструктуре и оптимизации распределенных сервисов.</p>
 
@@ -45,38 +54,40 @@ export default function Page() {
 изучаю новые технологии и инструменты, чтобы быть эффективнее в работе. Спокойный, исполнительный, 
 дружелюбный – умею работать как самостоятельно, так и в команде.
 </p>
-<p>Если вам нужен надежный backend-разработчик, буду рад пообщаться!</p>
+<p>Если вам нужен надежный разработчик, буду рад пообщаться!</p>
 {/* <VideoYoutube></VideoYoutube> */}
-
-<h3>Разработаю сайт/лендинг на React/Next.js </h3>
+<pre className="prose prose-neutral dark:prose-invert ">
+<h3>Разработаю сайт на React/Next.js </h3>
 {/* <p>Пару лет назад начал изучать и заниматься фронтенд разработкой, на текущий момент мне интересен React/Next.js. <br/> */}
-<p className="text-sm">PS:Этот сайт написан на Next.js / Tailwind CSS</p>
+<p className="text-sm">Этот [вы смотрите] написан на <Link href="https://ivanistereotekk.github.io/hidden-garden/"> React / Next.js / Tailwind CSS</Link></p>
         <p>
-          Люблю путешествовать и  
-            <a href="https://mixcult.bandcamp.com/album/self-object-id-ep"> музыку.</a> <br/>
+          И этот тоже написан на   
+            <Link href="https://ivanistereotekk.github.io/hidden-garden/"> React / Next.js / Tailwind.</Link> <br/>
 
         </p>
+</pre>
     
         <p>
           Если у вас возникла необходимость ознакомится с моим опытом, то по этой ссылке вы найдете мое{" "}
-          <a href="https://drive.google.com/file/d/1e0Ld62ct05Fld14XClFR2olxGQd92Zrd/view?usp=sharing">РЕЗЮМЕ</a>.
+          <Link href="https://drive.google.com/file/d/1e0Ld62ct05Fld14XClFR2olxGQd92Zrd/view?usp=sharing">РЕЗЮМЕ</Link>.
         </p>
         <p>
           Рекомендация с места работы{" "}
-          <a href="https://drive.google.com/file/d/1vmkdQpL6pjxc5wKelzC72vWGGa8ISG59/view?usp=drive_link">в этом письме.</a>.
+          <Link href="https://drive.google.com/file/d/1vmkdQpL6pjxc5wKelzC72vWGGa8ISG59/view?usp=drive_link">в этом письме.</Link>.
         </p>
 
         <p>
           English Version of my {" "}
-          <a href="https://drive.google.com/file/d/1MLT-dgXM2oEy2UY6ee7EwjIuxl4AAgnv/view?usp=sharing">Curriculum Vitae (CV).</a>.
+          <Link href="https://drive.google.com/file/d/1MLT-dgXM2oEy2UY6ee7EwjIuxl4AAgnv/view?usp=sharing">Curriculum Vitae (CV).</Link>.
         </p>
         <p>
           Если вам понадобится мой репозиторий в{" "}
-          <a href="https://github.com/ivanIStereotekk" target="_blank">
+          <Link href="https://github.com/ivanIStereotekk" target="_blank">
             GitHub
-          </a>
+          </Link>
           .
         </p>
+
 
         {/* <p>
           Telegram Бот ИИ (RAG) консультант по гражданским правам  {" "}
