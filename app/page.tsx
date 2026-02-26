c8bf07f41026f740397ab448f0a0865bdbab89f0
ewan <ewan@ewans-MacBook-Pro.local>
2026-02-26 17:00:01 +0300

    latest

--- app/page.tsx
+++ app/page.tsx
@@ -112,44 +112,6 @@ React / Next.js / Tailwind and more.<br/>
 </pre>
 
 <hr/>
-<pre className='prose prose-neutral dark:prose-invert text-sm'>
-<p>
-  Dear friends, sympathizers, and visitors to this cozy blog! 
-</p>
-Since April of last year, I've been searching for a job and any opportunity to earn money.
-I'm 46, have children, and don't own a home, so having a steady job is vital. Without a salary, 
-my life becomes extremely difficult. I can't afford paying rent, bills, or my children's needs, and 
-living without an income is simply impossible in my situation. <br/>
-Last April, I was forced to quit my job due to pressure from my colleagues, and the company before 
-my last job simply stopped paying me, citing legal proceedings and other nonsense as the reason. 
-The construction company I worked for still hasn't paid me three months' salary, and apparently has 
-no intention of doing so. <br/>
-As a colleague and employee, I'm diligent, I'm well versed in my profession, I enjoy it, and I work 
-very hard to be a professional, even though I never had a relevant education. I'm a self-taught 
-professional who enjoys working in IT. <br/>
-As I mentioned above, since April, I've responded to over 1,000 questions. After quitting my last job, 
-wanting to save a little money on housing, I moved to Tbilisi, Georgia, 
-the city I grew up in and love as a child. <br/>
-Housing in Tbilisi is cheaper than in Moscow, and without an income during my job search, 
-this has been a great saving and a great help for me. <br/>
-Perhaps I'm being overly suspicious, but I couldn't help but notice a stark contrast 
-in the response rate to my resumes and job postings. Almost immediately after leaving Russia, 
-all my responses were rejected. <br/>
-Although normally, my search was always lively, with communication, interesting interviews, 
-and test assignments, this was a stark contrast and alarming. <br/>
-I don't want to delve into conspiracy theories about my situation, but it was clear from 
-everything that my accounts were clearly limited. And upon returning to Moscow, I was finally convinced that finding a job, even offline, was simply impossible. <br/>
-No one was hiring me! Or they hire, and then suddenly don't, and there are variations. <br/>
-Judging by the behavior of the hiring managers—their sudden changes of heart, refusal to 
-answer the phone, and sometimes their mocking attitude—it makes me feel as if I'm in "development," 
-and they're pressuring me or simply bullying me. <br/>
-Perhaps this has to do with my attitude toward world events, my views on humanism, open education, 
-values, and Open Source, which I've sometimes moderately discussed on social media. 
-Perhaps there are other reasons I'm unaware of. I don't have a clear answer, only desperation and 
-a desire to end this! <br/>
-
-</pre>
-
 </div>
 </section>
 <hr/>
