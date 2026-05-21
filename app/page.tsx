a65770ba6f173c6a7e4b7f240637c1cddcbd3adc
Ivan Goncharov <telepop.iv@gmail.com>
2026-05-21 13:42:19 +0300

    photos added from there where nowhere is there

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Page() {
     <section>
       <a href={socialLinks.twitter} target="_blank">
         <Image
-          src="/public/pics/IMG_0134.jpg"
+          src="/pics/IMG_0134.jpg"
           alt="Profile photo"
           className="rounded-full bg-gray-100 block lg:mt-5 mt-0 lg:mb-5 mb-10 mx-auto sm:float-right sm:ml-5 sm:mb-5 grayscale hover:grayscale-0"
           unoptimized
