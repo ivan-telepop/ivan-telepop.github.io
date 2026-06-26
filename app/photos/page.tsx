5db8b101d11480a1efad0d9fbc937f124a4ed06a
Ivan Goncharov <telepop.iv@gmail.com>
2026-06-26 21:19:28 +0300

    pics reorganised

--- app/photos/page.tsx
+++ app/photos/page.tsx
@@ -15,59 +15,33 @@ export default function Photos() {
         columns={3}
         images={[
           {
-            src: "/pics/IMG_0001.png",
+            src: "/pics/plane.jpg",
             alt: "Posing well",
             href: "https://ableton.com",
           },
           {
-            src: "/photos/pic1.jpg",
+            src: "/pics/ableton.png",
             alt: "Home Studio",
             href: "https://mixcult.bandcamp.com/album/self-object-id-ep",
-          },
+          },          
           {
-            src: "/pics/IMG_0002.png",
-            alt: "code",
-            href: "https://github.com/ivanIStereotekk",
-          },
-          {
-            src: "/pics/IMG_0003.png",
-            alt: "Leto",
-            href: "https://github.com/ivanIStereotekk",
-          },
-          {
-            src: "/pics/IMG_0004.png",
-            alt: "Graph",
-            href: "https://github.com/ivanIStereotekk",
-          },
-          
-          {
-            src: "/pics/IMG_0005.png",
-            alt: "Tbilisi",
-            href: "https://github.com/ivanIStereotekk",
-          },
-          {
-            src: "/pics/IMG_0006.png",
+            src: "/pics/tree.jpg",
             alt: "tree",
             href: "https://github.com/ivanIStereotekk",
           },
           {
-            src: "/pics/IMG_0007.png",
+            src: "/pics/studio.jpg",
             alt: "tree",
             href: "https://github.com/ivanIStereotekk",
           },
           {
-            src: "/pics/IMG_1.JPG",
+            src: "/pics/vs-code.png",
             alt: "tree",
             href: "https://github.com/ivanIStereotekk",
           },
           {
-            src: "/pics/IMG_2.JPG",
-            alt: "tree",
-            href: "https://github.com/ivanIStereotekk",
-          },
-          {
-            src: "/pics/IMG_3.JPG",
-            alt: "tree",
+            src: "/pics/fastapi.jpg",
+            alt: "workspace",
             href: "https://github.com/ivanIStereotekk",
           },
         ]}
@@ -76,37 +50,25 @@ export default function Photos() {
       <ImageGrid
         columns={4}
         images={[
-          { src: "/pics/IMG_1.JPG", alt: "Posing" },
-          { src: "/pics/IMG_2.JPG", alt: "Tree" },
-          { src: "/pics/IMG_0001.png", alt: "Studio" },
-          { src: "/pics/IMG_0002.png", alt: "Ivan" },
-          { src: "/pics/IMG_0003.png", alt: "Ableton" },
-          { src: "/pics/IMG_0004.png", alt: "VS-Code" },
-          { src: "/pics/IMG_0005.png", alt: "Plane" },
-          { src: "/pics/IMG_0006.png", alt: "null" },
-          { src: "/pics/IMG_0007.png", alt: "Plane" },
-          { src: "/pics/IMG_3.JPG", alt: "Plane" },
-          { src: "/pics/IMG_1.JPG", alt: "Posing" },
-          { src: "/pics/IMG_2.JPG", alt: "Tree" },
-          { src: "/pics/IMG_0001.png", alt: "Studio" },
-          { src: "/pics/IMG_0002.png", alt: "Ivan" },
-          { src: "/pics/IMG_0003.png", alt: "Ableton" },
-          { src: "/pics/IMG_0004.png", alt: "VS-Code" },
-          { src: "/pics/IMG_0005.png", alt: "Plane" },
-          { src: "/pics/IMG_0006.png", alt: "null" },
-          { src: "/pics/IMG_0007.png", alt: "Plane" },
-          { src: "/pics/IMG_3.JPG", alt: "Plane" },
+          { src: "/pics/fastapi.jpg", alt: "Posing" },
+          { src: "/pics/ableton.png",alt: "Ivan" },
+          { src: "/pics/vs-code.png", alt: "VS-Code" },
+          { src: "/pics/glasses.jpg", alt: "Tree" },
+          { src: "/pics/fastapi.jpg", alt: "Studio" },
+          { src: "/pics/plane.jpg", alt: "Ableton" },
+          { src: "/pics/studio.jpg", alt: "VS-Code" },
+          { src: "/pics/tree.png", alt: "Plane" },
+          { src: "/pics/vs-code.png", alt: "null" },
         ]}
       />
 
       <ImageGrid
         columns={4}
         images={[
-          { src: "/photos/pic1.jpg", alt: "***" },
+          { src: "/photos/fastapi.jpg", alt: "***" },
           { src: "/photos/ableton.png", alt: "me" },
-          { src: "/photos/dog.jpg", alt: "Dog" },
           { src: "/photos/vs-code.png", alt: "eq" },
-          { src: "/photos/fl-02.jpg", alt: "Plane" },
+          { src: "/photos/studio.jpg", alt: "Plane" },
           { src: "/photos/tree.png", alt: "Tree" },
         ]}
       />
